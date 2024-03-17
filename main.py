fa3237368799a85a0af89cc3727f8bc95ef8201d
Mingxi Lv <uke_sjtu@sjtu.edu.cn>
2024-03-17 23:50:52 +0800

    feat-1: init pygame with black screen

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 import pygame
+
+# initialize the pygame
+pygame.init()
+
+# set window size and name
+screen_width, screen_height = 800, 800
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption("UserName-蛇吃豆-UserID")
