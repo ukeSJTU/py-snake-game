c1c475a934c0b429c92757bbca937349f645b24e
Mingxi Lv <uke_sjtu@sjtu.edu.cn>
2024-03-17 23:58:43 +0800

    feat-2: init snake at center of screen

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ screen_width, screen_height = 800, 800
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("UserName-蛇吃豆-UserID")
+
+# draw snake
+pygame.draw.rect(surface=screen, color=(255, 255, 255), rect=(400, 400, 20, 10))
+pygame.display.update()
