f0e9a45566c73f2bc2198820a8d1a189a05b7bb8
Mingxi Lv <uke_sjtu@sjtu.edu.cn>
2024-03-18 09:48:51 +0800

    feat: add color alias

---
+++ colors.py
@@ -0,0 +1,9 @@
+# defines the colors used in the game
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+YELLOW = (255, 255, 102)
+RED = (213, 50, 80)
+ORANGE = (255, 165, 0)
+BLUE = (50, 153, 213)
+GREEN = (0, 255, 0)
