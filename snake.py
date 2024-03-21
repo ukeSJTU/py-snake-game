eca308d67fe98cd7f7d8c6ac399cd4b7e54982fa
Mingxi Lv <uke_sjtu@sjtu.edu.cn>
2024-03-21 11:33:05 +0800

    chore: add comment for snake.py

--- snake.py
+++ snake.py
@@ -1,10 +1,8 @@
 from typing import List, Tuple, Union, Literal
 from colors import *
 import pygame
-
 from base_class import Block
 from type_alias import *
-
 
 Positon = Tuple[int, int]
 Color = Tuple[int, int, int]
@@ -12,12 +10,26 @@ Color = Tuple[int, int, int]
 
 class SnakeBodyBlock:
     def __init__(self, pos: Positon, color: Color, width: int, height: int):
+        """Body block of the snake
+
+        Args:
+            pos (Positon): position of the snake body block (px)
+            color (Color): color of the snake body block
+            width (int): width of the snake body block (px)
+            height (int): height of the snake body block (px)
+        """
         self.pos = pos
         self.color = color
         self.width = width
         self.height = height
 
     def draw(self: "Block", screen: pygame.Surface):
+        """Draw the block on the screen
+
+        Args:
+            self (Block): block
+            screen (pygame.Surface): screen to draw the block
+        """
         pygame.draw.rect(
             screen,
             self.color,
@@ -31,6 +43,12 @@ class Snake:
         body: List[SnakeBodyBlock],
         direction: Direction,
     ):
+        """__init__ method for Snake Class
+
+        Args:
+            body (List[SnakeBodyBlock]): the body of the snake
+            direction (Direction): the direction of the snake
+        """
         self.body = body
         self.direction = direction
 
@@ -39,12 +57,32 @@ class Snake:
         self.isGrowing = False
 
     def get_direction(self: "Snake") -> Direction:
+        """Get the direction of the snake
+
+        Args:
+            self (Snake): Snake object
+
+        Returns:
+            Direction: direction of the snake
+        """
         return self.direction
 
     def set_direction(self: "Snake", direction: Direction) -> None:
+        """Set the direction of the snake
+
+        Args:
+            self (Snake): Snake object
+            direction (Direction): direction to set
+        """
         self.direction = direction
 
     def move(self: "Snake", distance: int = 10):
+        """Move the snake in the current direction
+
+        Args:
+            self (Snake): Snake object
+            distance (int, optional): distance to move. Defaults to 10.
+        """
         self.head_pos = self.body[-1].pos
         if self.direction == "UP":
             self.head_pos = (self.head_pos[0], self.head_pos[1] - distance)
@@ -73,16 +111,46 @@ class Snake:
             self.isGrowing = False
 
     def get_head_pos(self: "Snake") -> Positon:
+        """Get the position of the snake's head
+
+        Args:
+            self (Snake): Snake object
+
+        Returns:
+            Positon: position of the snake's head
+        """
         return self.head_pos
 
     def get_all_pos(self: "Snake") -> List[Positon]:
+        """Get the positions of all blocks in the snake's body
+
+        Args:
+            self (Snake): Snake object
+
+        Returns:
+            List[Positon]: list of positions of all blocks in the snake's body
+        """
         return [block.pos for block in self.body]
 
     def grow(self: "Snake", color: Color = WHITE, width: int = 10, height: int = 10):
+        """Make the snake grow by adding a new block
+
+        Args:
+            self (Snake): Snake object
+            color (Color, optional): color of the new block. Defaults to WHITE.
+            width (int, optional): width of the new block (px). Defaults to 10.
+            height (int, optional): height of the new block (px). Defaults to 10.
+        """
         self.isGrowing = True
         self.move()
         self.body[0].color = color
 
     def draw(self: "Snake", screen: pygame.Surface):
+        """Draw the snake on the screen
+
+        Args:
+            self (Snake): Snake object
+            screen (pygame.Surface): screen to draw the snake
+        """
         for block in self.body:
             block.draw(screen)
